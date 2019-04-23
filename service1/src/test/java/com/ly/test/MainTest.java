65c7e8ed57434d4c1684d5ebb9eaf973b120def4
yattiecrui <5422063@qq.com>
2019-04-23 22:57:38 +0800

    aaa

---
+++ service1/src/test/java/com/ly/test/MainTest.java
@@ -0,0 +1,24 @@
+package com.ly.test;
+
+import com.ly.service1.Service1Application;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Service1Application.class)
+public class MainTest {
+
+    @Resource
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void encrypt(){
+        String a = stringEncryptor.encrypt("Liuyong123!");
+        System.out.println(a);
+    }
+}
